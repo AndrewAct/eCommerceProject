ce10c33fe32fbe78782d450287dddbcc0cfb1409
Andrew Chen <andrew47ohio@gmail.com>
2021-01-05 14:15:34 -0500

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+//<!-- Even if you don't want a "div", the empty fragment is required! -->
+
+import React from 'react';
+
+function App() {
+  return (  
+    <>
+      <h1>Welcome to MyShop!</h1>
+    </>
+  )
+}
+
+export default App
