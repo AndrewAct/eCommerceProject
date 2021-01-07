ded0cceed4aa4f5e430feb1f49985224d1cb6916
Andrew Chen <andrew47ohio@gmail.com>
2021-01-06 21:34:58 -0500

    Screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,26 @@
 //<!-- Even if you don't want a "div", the empty fragment is required! -->
 
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (  
-    <>
+    <Router>
       <Header />
       <main className = 'py-3'>
       <Container>
         <h1>Welcome to MyShop!</h1>
-        <HomeScreen />
+        <Route path = '/' component = {HomeScreen} exact/>
+        <Route path = '/product/:id' component = {ProductScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
